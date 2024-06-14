25834b58ed0a821092e6b8e17a10b03ef204e7b3
Gabriel Dorneles Rebello <130177863+Rebellows@users.noreply.github.com>
2024-06-14 08:47:43 -0300

    Update siren_generator.v

--- CircuitosDigitais/6- automotiveAntiTheftSystem/siren_generator.v
+++ CircuitosDigitais/6- automotiveAntiTheftSystem/siren_generator.v
@@ -2,16 +2,26 @@ module siren_generator (
     input clock, reset,
     input two_hz_enable,
     input enable_siren,
-    output reg [1:0] siren
+    output reg [2:0] siren
 );
+
+reg count;
 
 always @(posedge clock, posedge reset) begin
     if (reset || !enable_siren) begin
-        siren <= 2'b00;
+        siren <= 3'b000;
+        count <= 1'b0;
     end
     else begin
         if (two_hz_enable) begin
-            siren <= ~siren;
+            if (!count) begin
+                siren <= 3'b001;
+                count <= 1'b1;
+            end
+            else begin
+                siren <= 3'b100;
+                count <= 1'b0;
+            end
         end
     end
 end
