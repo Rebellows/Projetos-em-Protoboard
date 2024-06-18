7090a98ee05a7f041bc85f17daca011db4f8fa23
Gabriel Dorneles Rebello <130177863+Rebellows@users.noreply.github.com>
2024-06-18 09:24:20 -0300

    Update timer.v

--- CircuitosDigitais/6- automotiveAntiTheftSystem/timer.v
+++ CircuitosDigitais/6- automotiveAntiTheftSystem/timer.v
@@ -8,11 +8,12 @@ module timer (
     reg [26:0] counter_1hz;  
     reg [25:0] counter_2hz;  
     reg [3:0] timer_count;
+    reg aux;
 
     parameter ONE_HZ_MAX = 100_000_000;  
     parameter TWO_HZ_MAX = 50_000_000; 
 
-    always @(posedge clock or posedge reset) begin
+    always @(posedge clock, posedge reset) begin
         if (reset) begin
             counter_1hz <= 0;
             counter_2hz <= 0;
@@ -20,15 +21,8 @@ module timer (
             two_hz_enable <= 0;
             expired <= 0;
             timer_count <= 0;
+            aux;
         end 
-        else if (start_timer) begin
-            counter_1hz <= 0;
-            counter_2hz <= 0;
-            one_hz_enable <= 0;
-            two_hz_enable <= 0;
-            expired <= 0;
-            timer_count <= value;
-        end
         else begin
             if (counter_1hz >= ONE_HZ_MAX - 1) begin
                 counter_1hz <= 0;
@@ -46,12 +40,22 @@ module timer (
                 counter_2hz <= counter_2hz + 1;
                 two_hz_enable <= 1'b0;
             end
+        end
+    end
+
+    always @(posedge clock, posedge reset) begin
+        if (start_timer) begin
+            if (!aux) begin
+               timer_count <= value;
+               aux <= 1'b1;
+            end
             if (one_hz_enable) begin
-                if (timer_count > 0) begin
-                    timer_count <= timer_count - 1;
+                if (timer_count <= 4'b0000) begin
+                    expired <= 1'b1; 
+                    aux <= 1'b0;     
                 end
                 else begin
-                    expired <= 1'b1;
+                    timer_count <= timer_count - 1'b1;
                 end
             end
         end
