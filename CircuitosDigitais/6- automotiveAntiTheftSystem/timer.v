42e7235fa00c08685b64b0fda920adbbae433673
Gabriel Dorneles Rebello <130177863+Rebellows@users.noreply.github.com>
2024-06-18 19:05:08 -0300

    Update timer.v

--- CircuitosDigitais/6- automotiveAntiTheftSystem/timer.v
+++ CircuitosDigitais/6- automotiveAntiTheftSystem/timer.v
@@ -2,18 +2,17 @@ module timer (
     input clock, reset,
     input [3:0] value,
     input start_timer,
-    output reg one_hz_enable, two_hz_enable, expired
+    output reg one_hz_enable, two_hz_enable, expired,
+    output reg [3:0] timer_count
 );
 
     reg [26:0] counter_1hz;  
     reg [25:0] counter_2hz;  
-    reg [3:0] timer_count;
-    reg aux;
 
     parameter ONE_HZ_MAX = 100_000_000;  
     parameter TWO_HZ_MAX = 50_000_000; 
 
-    always @(posedge clock, posedge reset) begin
+    always @(posedge clock or posedge reset) begin
         if (reset) begin
             counter_1hz <= 0;
             counter_2hz <= 0;
@@ -21,8 +20,15 @@ module timer (
             two_hz_enable <= 0;
             expired <= 0;
             timer_count <= 0;
-            aux <= 0;
         end 
+        else if (start_timer) begin
+            counter_1hz <= 0;
+            counter_2hz <= 0;
+            one_hz_enable <= 0;
+            two_hz_enable <= 0;
+            expired <= 0;
+            timer_count <= value;
+        end
         else begin
             if (counter_1hz >= ONE_HZ_MAX - 1) begin
                 counter_1hz <= 0;
@@ -40,23 +46,13 @@ module timer (
                 counter_2hz <= counter_2hz + 1;
                 two_hz_enable <= 1'b0;
             end
-        end
-    end
-
-    always @(posedge clock, posedge reset) begin
-        if (start_timer) begin
-            if (!aux) begin
-               timer_count <= value;
-               aux <= 1'b1;
-            end
             if (one_hz_enable) begin
-                if (timer_count <= 4'b0000) begin
-                    expired <= 1'b1; 
-                    aux <= 1'b0;     
+                if (timer_count > 0) begin
+                    timer_count <= timer_count - 1;
                 end
-                else begin
-                    timer_count <= timer_count - 1'b1;
-                end
+            end
+            else if (timer_count == 0) begin
+                expired <= 1'b1;
             end
         end
     end
