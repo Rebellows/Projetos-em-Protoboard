6c244e87dfcb229e5a3e2bf2a5d47c101ba380be
Gabriel Dorneles Rebello <130177863+Rebellows@users.noreply.github.com>
2024-07-05 08:12:50 -0300

    Update dcm.v

--- CircuitosDigitais/T3SistDigitais/dcm.v
+++ CircuitosDigitais/T3SistDigitais/dcm.v
@@ -14,7 +14,7 @@ parameter CLOCK_5 = ONE_HZ / 0.3125;
 parameter CLOCK_6 = ONE_HZ / 0.15625;
 parameter CLOCK_7 = ONE_HZ / 0.078125;
 
-reg [31:0] counter, aux;
+reg [31:0] counter_1, counter_2, aux;
 
 always @(posedge clock or posedge reset) begin
     if (reset) begin
@@ -22,60 +22,60 @@ always @(posedge clock or posedge reset) begin
         prog_out <= 3'b0;
     end
     else begin
-        if (counter == aux) begin
-            counter <= 32'b0;
+        if (counter_2 == aux) begin
+            counter_2 <= 32'b0;
             clock_2 <= 1'b1;
         end
-        else if (counter == CLOCK_0) begin
+        else if (counter_1 == CLOCK_0) begin
+            counter_1 <= 32'b0;
             clock_1 <= 1'b1;
         end
-        counter <= counter + 1'b1;
+        counter_2 <= counter_2 + 1'b1;
+        counter_1 <= counter_1 + 1'b1;
         clock_2 <= 1'b0;
         clock_1 <= 1'b0;
-    end
-end
+        if (update) begin
+            case (prog_in)
 
-always @(*) begin
-    if (update) begin
-        case (prog_in)
+                3'b000: begin
+                    aux <= CLOCK_0;
+                end
 
-            3'b000: begin
-                aux = CLOCK_0;
-            end
+                3'b001: begin
+                    aux <= CLOCK_1;
+                end
 
-            3'b001: begin
-                aux = CLOCK_1;
-            end
+                3'b010: begin
+                    aux <= CLOCK_2;                
+                end
 
-            3'b010: begin
-                aux = CLOCK_2;                
-            end
+                3'b011: begin
+                    aux <= ONE_HZ;
+                end
 
-            3'b011: begin
-                aux = ONE_HZ;
-            end
+                3'b100: begin
+                    aux <= CLOCK_4;
+                end
 
-            3'b100: begin
-                aux = CLOCK_4;
-            end
+                3'b101: begin
+                    aux <= CLOCK_5;
+                end
 
-            3'b101: begin
-                aux = CLOCK_5;
-            end
+                3'b110: begin
+                    aux <= CLOCK_6;
+                end
 
-            3'b110: begin
-                aux = CLOCK_6;
-            end
+                3'b111: begin
+                    aux <= CLOCK_7;
+                end
 
-            3'b111: begin
-                aux = CLOCK_7;
-            end
+                default: begin
+                    aux <= CLOCK_0;
+                end
 
-            default: begin
-                aux = CLOCK_0;
-            end
-
-        endcase
+            endcase
+            counter_2 <= 32'b0;
+        end        
     end
 end
 
